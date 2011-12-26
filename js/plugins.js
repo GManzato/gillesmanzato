b8c24c36969add2a3ab495b87c0857e4e15a2d8c
Gilles Manzato <gmanzato@dcf-services.com>
2011-12-26 23:20:22 +0100

    V2 Website

--- js/plugins.js
+++ js/plugins.js
@@ -18,6 +18,36 @@ window.log = function(){
   }
 };
 
+function linkify(txt) {
+	var regexp = /((ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?)/gi;
+	return txt.replace(regexp, '<a href="$1">$1</a>');
+}
+/* time left function for twitter widget*/
+function relative_time(time_value) {
+	var values = time_value.split(" ");
+	time_value = values[1] + " " + values[2] + ", " + values[5] + " " + values[3];
+	var parsed_date = Date.parse(time_value);
+	var relative_to = (arguments.length > 1) ? arguments[1] : new Date();
+	var delta = parseInt((relative_to.getTime() - parsed_date) / 1000);
+	delta = delta + (relative_to.getTimezoneOffset() * 60);
+	var r = '';
+	if (delta < 60) {
+		r = 'a minute ago';
+	} else if (delta < 120) {
+		r = 'couple of minutes ago';
+	} else if (delta < (45 * 60)) {
+		r = (parseInt(delta / 60)).toString() + ' minutes ago';
+	} else if (delta < (90 * 60)) {
+		r = 'an hour ago';
+	} else if (delta < (24 * 60 * 60)) {
+		r = '' + (parseInt(delta / 3600)).toString() + ' hours ago';
+	} else if (delta < (48 * 60 * 60)) {
+		r = '1 day ago';
+	} else {
+		r = (parseInt(delta / 86400)).toString() + ' days ago';
+	}
+	return r;
+}
 
 
 // catch all document.write() calls
--- js/script.js
+++ js/script.js
@@ -6,92 +6,20 @@
 
 $(function(){
 	
-	 $('#container nav').localScroll({hash:true,});
+	 /*$('#container nav').localScroll({hash:true,});
 	
-	offset = $('#container nav#main').offset()
-	bodyEl = $('body');
 	 $(window).scroll(function () { 
 	 	if(offset.top < bodyEl.scrollTop()+20)
 		 	$('#container nav#main').css('top',bodyEl.scrollTop()+20);
 		else 
 			$('#container nav#main').css('top',offset.top);
 	 });
-	 
-	skills = {
-	 "Code": {
-		"HTML5":1,
-		"jQuery":0.8,
-		"CSS3": 1,
-		"PHP": 0.5,
-		"SEO": 0.5,
-		"Mail": 0.5,
-   		},
-	"Design": {
-		"photoshop":0.8,
-		"illustrator":0.6,
-		},
-	"CMS":{
-		"Wordpress":0.8,
-		"Magento":0.4,
-		"Sitecore":0.2
-		}
- 	}
-	
-	var slist = $('.slist');
-	
-	$.each(skills,function(index){
-		slist.append('<a data-skill="'+index+'" href="#skills">'+index+'</a');
-	});
-	function htmlSkills(){
-		var div = '<div class="skill">';
-		div +='   <span class="skillName">Javascript</span>';
-		div +='   <div class="skillContainer">';
-		div +='  	<div class="skillContent"></div>';
-		div +='	  </div>';
-		div +='</div>';
-		return div
-	}
-	function renderSkills(skill){
-		$('.slist a').removeClass("selected");
-		$('.slist a[data-skill="'+skill+'"]').addClass('selected');
-		skillsContainer = $('.skillsContainer');
-		$('.skillName').animate({opacity:0},500);
-		$('.skillContent').animate({width:'0%'},500,function(){
-			$('.skillsContainer').empty();
-			$.each(skills[skill],function(name,size){
-				var div = htmlSkills();
-				result = $('.skillsContainer').append(div);
-				$('.skillName:last',result).html(name).hide().fadeIn();
-				$('.skillContent:last',result).animate({width:(size*100)+"%",opacity:size},2000,'easeOutElastic');			
-			});			
-		});
-	};
-	
-	$('#contact .form form').submit(function(e){
-		e.preventDefault();
-		$('#response').html('En cours <img src="img/ajax-loader.gif" />');
-		$.post("send.php" , $("#contact .form form").serialize(),function(data){
-			if(data.success)
-				{
-					$('#response').html(data.text);
-					$("#contact .form form").slideUp('slow');
-				}
-			else {
-					$('#response').html(data.text);
-				}
-			},'json');
-	});
-	
-	renderSkills($('.slist a:first').attr("data-skill"));
-	
-	$('.slist a').live('click',function(e){
-		e.preventDefault();
-		renderSkills($(this).attr("data-skill"));	
-	});
-	$.getJSON("http://api.twitter.com/1/statuses/user_timeline.json?screen_name=gillesm&count=1&callback=?",function(data){
-		$('#lastTweet').html(data[0].text);
-	});
-	//
+*/
+$.getJSON('http://twitter.com/statuses/user_timeline/gillesm.json?callback=?', function(data) {
+				var html = "<b><a href='http://twitter.com/gillesm/status/" + data[0].id_str + "'>" + relative_time(data[0].created_at) + "</a></b> " + linkify(data[0].text);
+				$("#lastTweet").html(html);
+			});
+
 	twitpic.users.show({'username':'gillesm','page':'1'}, function(user) {
 		$('#lastPict').html('<img src="'+'http://twitpic.com/show/thumb/'+user.images[0].short_id+'" />');
 	});
